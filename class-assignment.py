508e2b69ebdb87eb1a70623840a684eac97c4f26
Ny <nyarce805@gmail.com>
2022-02-02 13:33:35 -0600

    Updated

--- class-assignment.py
+++ class-assignment.py
@@ -3,17 +3,44 @@
 
 #defines the parent class
 class Media:
-    name = ""
-    upc = ""
-    release = ""
+    name = "Media Name Goes Here"
+    upc = "35416548"
+    release = "02/02/22"
+    
+    def getName(self):
+        msg = "\nMedia name: {}.".format(self.name)
+        return msg
+    
+    #basic method that pulls name into var
 
 #defines first child class & attributes    
 class Music(Media):
-    artist = ""
-    format = ""
+    artist = "Nine Inch Nails"
+    format = "vinyl"
+    
+    def getName(self):
+        msg = "\nArtist name: {}.".format(self.artist)
+        return msg
+
+    #modify method to pull artist
 
 
 #defines second child class & attributes    
 class Book(Media):
-    author = ""
-    publisher = ""
+    author = "Some Author"
+    publisher = "Book co"
+    
+    def getName(self):
+        msg = "\nAuthor name: {}.".format(self.author)
+        return msg
+    #modufy method to pull author
+    
+if __name__ == "__main__":
+    media = Media()
+    print(media.getName())
+    music = Music()
+    print(music.getName())
+    book = Book()
+    print(book.getName())
+
+
