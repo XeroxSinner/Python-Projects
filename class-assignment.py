663621753ff06152948dcea62817a91ccb5c84c1
Ny <nyarce805@gmail.com>
2022-02-02 13:00:35 -0600

    Small edit

--- class-assignment.py
+++ class-assignment.py
@@ -1,4 +1,5 @@
 #Two child classes, separate attribs.
+#Add method to parent class - "Both child classes should utilize polymorphism on the parent class method."
 
 #defines the parent class
 class Media:
